4a390cc6b29bf4783d170e6a974efbfc5dd71667
fnobi <shin@fnobi.com>
2012-12-11 15:57:47 +0900

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hebigara::Application.config.secret_token = '4facb411cb546b84f13b86d10eeaeef767401925fb12e0994fee858222cd1841af00efa9e5a57364e5f31d40692a64f1feb74b4b6d5cebaecc1738588c2393a0'
